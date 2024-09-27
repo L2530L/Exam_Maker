b41844e53b91a58444e56af4272e1c895884adf7
L2530L <elagrosa23A-0057@olopsc.edu.ph>
2024-09-27 14:32:11 +0800

    add detail tabs

---
+++ lib/components/details.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class Details extends StatelessWidget {
+  final List<DropdownMenuEntry> entries;
+  final String label;
+  final TextEditingController controller;
+
+  const Details({
+    super.key,
+    required this.label,
+    required this.entries,
+    required this.controller
+  });
+
+  check(value){
+    
+  }
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownMenu(
+      label: Text(label),
+      dropdownMenuEntries: entries,
+      menuHeight: 234,
+      width: 250,
+      controller: controller,
+    );
+  }
+}
